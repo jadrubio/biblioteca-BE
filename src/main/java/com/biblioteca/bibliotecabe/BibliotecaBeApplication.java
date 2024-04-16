75ece6c20bb0a4a79f35a131dfbe0015b4b230e9
Jose Rubio <jadrubio@gmail.com>
2024-04-16 15:47:02 -0400

    init commit

---
+++ src/main/java/com/biblioteca/bibliotecabe/BibliotecaBeApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.bibliotecabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblioteca/bibliotecabe/BibliotecaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.bibliotecabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
